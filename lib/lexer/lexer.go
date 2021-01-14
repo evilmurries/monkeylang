488672801c37076bf7db4ba25bfebd8882334704
evilmurries <cpiwarski00@gmail.com>
2021-01-13 18:45:51 -0800

    lexer: Passes test

--- lib/lexer/lexer.go
+++ lib/lexer/lexer.go
@@ -2,7 +2,6 @@ package lexer
 
 import (
 	"github.com/evilmurries/monkeylang/lib/token"
-	"testing"
 )
 
 type Lexer struct {
@@ -59,38 +58,4 @@ func (l *Lexer) NextToken() token.Token {
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
-}
-
-func TestNextToken(t *testing.T) {
-	input := `=(){},;`
-
-	tests := []struct {
-		expectedType    token.TokenType
-		expectedLiteral string
-	}{
-		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
-		{token.LPAREN, "("},
-		{token.RPAREN, ")"},
-		{token.LBRACE, "{"},
-		{token.RBRACE, "}"},
-		{token.COMMA, ","},
-		{token.SEMICOLON, ";"},
-		{token.EOF, ""},
-	}
-	l := New(input)
-
-	for i, tt := range tests {
-		tok := l.NextToken()
-
-		if tok.Type != tt.expectedType {
-			t.Fatalf("tests[%d]- tokentype wrong. expected=%q, got=%q",
-				i, tt.expectedType, tok.Type)
-		}
-
-		if tok.Literal != tt.expectedLiteral {
-			t.Fatalf("tests[%d] - literal wrong, expected=%q, gpt=%q",
-				i, tt.expectedLiteral, tok.Literal)
-		}
-	}
 }
